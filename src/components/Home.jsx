b40a844941163cc5f1e2ddb89a95c79d7999ab2d
kalbek <access2kaleb@gmail.com>
2023-10-24 13:49:20 +0300

    Modify headline sentence.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,22 +5,22 @@ const Home = () => (
     <div className="flex-column-start">
       <div className="content p-5">
         <div className="sub-title">
-          <h3>Welcome to our page!</h3>
+          <h3>An enchanting world of numbers designed just for kids!!</h3>
         </div>
         <div className="first-paragraph">
           Welcome to Math Magicians, where learning math becomes a thrilling adventure
-          for kids! Our enchanting website not only offers a handy inbuilt calculator 
-          but also serves up a daily dose of motivation with inspiring quotes. 
-          Join us on a journey where numbers turn into magic, and math becomes your superpower. 
+          for kids! Our enchanting website not only offers a handy inbuilt calculator
+          but also serves up a daily dose of motivation with inspiring quotes.
+          Join us on a journey where numbers turn into magic, and math becomes your superpower.
           Get ready to explore, calculate, and conquer with Math Magicians!
         </div>
         <br />
         <div className="second-pareagraph">
-          Math Magicians isn't just about solving equations; it's a place where young minds
-          discover the wonder in numbers. Our interactive games and educational resources 
-          make math not only fun but also an empowering tool for young learners.
+          Math Magicians isn&apos;t just about solving equations; it&apos;s a place where
+          young mindsdiscover the wonder in numbers. Our interactive games and educational
+          resources make math not only fun but also an empowering tool for young learners.
           As they explore the world of Math Magicians, children will build a strong foundation
-          for a lifetime of confidence in math, helping them unlock new opportunities 
+          for a lifetime of confidence in math, helping them unlock new opportunities
           and embrace the world with a smile.
         </div>
       </div>
